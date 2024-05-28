c43e3942b34ddce1b4eb15c47498b98277a2924d
Alina <alinash1436632.92@gmail.com>
2024-05-28 23:45:05 +0300

    Fix linter errors

--- homework7_cows_and_bulls.py
+++ homework7_cows_and_bulls.py
@@ -1,17 +1,18 @@
 """Module providing a function printing python version."""
 import random
+from typing import List
 
 print("Добро пожаловать в игру 'Быки и коровы'!")
 print("Компьютером загадано 4-значное число с неповторяющимися цифрами:")
 
-list_with_random_numbers = []
+list_with_random_numbers: List[int] = []
 while len(list_with_random_numbers) != 4:
     random_element = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
     if random_element not in list_with_random_numbers:
         list_with_random_numbers.append(random_element)
 
-common_cows = []
-common_bulls = []
+common_cows: List[int] = []
+common_bulls: List[int] = []
 while True:
     common_cows.clear()
     common_bulls.clear()
