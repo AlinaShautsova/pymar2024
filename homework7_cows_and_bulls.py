b09b33945bd538993d91d0478558eb62abef01bb
Alina <alinash1436632.92@gmail.com>
2024-05-28 23:36:29 +0300

    Fix linter errors

--- homework7_cows_and_bulls.py
+++ homework7_cows_and_bulls.py
@@ -4,7 +4,7 @@ import random
 print("Добро пожаловать в игру 'Быки и коровы'!")
 print("Компьютером загадано 4-значное число с неповторяющимися цифрами:")
 
-list_with_random_numbers = list()
+list_with_random_numbers = []
 while len(list_with_random_numbers) != 4:
     random_element = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
     if random_element not in list_with_random_numbers:
@@ -17,19 +17,19 @@ while True:
     common_bulls.clear()
     str_from_player = input("Назовите ваше число: ")
 
-    list_from_player = list()
+    list_from_player = []
     for char in str_from_player:
         list_from_player.append(int(char))
 
     for item in list_from_player:
         if item in list_with_random_numbers:
             common_cows.append(item)
-    print("{} Коровы".format(len(common_cows)))
+    print(f"{len(common_cows)} Коровы")
 
     for i in range(4):
         if list_from_player[i] == list_with_random_numbers[i]:
             common_bulls.append(list_from_player[i])
-    print("{} Быки".format(len(common_bulls)))
+    print(f"{len(common_bulls)} Быки")
 
     if common_cows == common_bulls:
         print("Поздравляем! Вы выиграли!")
--- homework7_pyramida.py
+++ homework7_pyramida.py
@@ -2,11 +2,12 @@
 
 
 def print_pyramid(a):
+    """Print pyramid by amount of lines."""
     for i in range(a):
         spaces = ' ' * (a - i - 1)
         stars = '*' * (2 * i + 1)
         print(spaces + stars + spaces)
 
 
-n = int(input("Какое количество n Вы хотите: "))
+n = int(input("What amount of n do you want: "))
 print_pyramid(n)
