5f732329159c8cb3d8801744d5a0586c20705314
Alina <alinash1436632.92@gmail.com>
2024-05-30 18:57:37 +0300

    Added check for unique numbers and changed count of cows

--- homework7_cows_and_bulls.py
+++ homework7_cows_and_bulls.py
@@ -9,6 +9,7 @@ while len(list_with_random_numbers) != 4:
     random_element = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
     if random_element not in list_with_random_numbers:
         list_with_random_numbers.append(random_element)
+print(list_with_random_numbers)
 
 common_cows: List[int] = []
 common_bulls: List[int] = []
@@ -16,21 +17,25 @@ while True:
     common_cows.clear()
     common_bulls.clear()
     str_from_player = input("Enter you prediction: ")
+    if (len(str_from_player) != len(set(str_from_player)) or
+            len(str_from_player) != 4):
+        print("Numbers must be unique and contains 4 integers")
+        continue
 
     list_from_player = []
     for char in str_from_player:
         list_from_player.append(int(char))
-
-    for item in list_from_player:
-        if item in list_with_random_numbers:
-            common_cows.append(item)
-    print(f"{len(common_cows)} Cows")
 
     for i in range(4):
         if list_from_player[i] == list_with_random_numbers[i]:
             common_bulls.append(list_from_player[i])
     print(f"{len(common_bulls)} Bulls")
 
-    if common_cows == common_bulls:
+    for item in list_from_player:
+        if item in list_with_random_numbers:
+            common_cows.append(item)
+    print(f"{len(common_cows) - len(common_bulls)} Cows")
+
+    if len(common_bulls) == 4:
         print("Congratulations! You are winner!")
         break
